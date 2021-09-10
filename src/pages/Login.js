63aef28d7e3c863c31ea87f2c5051481b51a17ac
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-10 18:59:24 +0200

    add login och register comp

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,71 @@
-import React, {useState} from 'react'
-import {useHistory} from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { signIn, signUpProvider, forgotPassword } from "../auth/firebase";
+
 
 const Login = () => {
-    const history = useHistory();
-    const [email, setEmail] = useState("");
-    console.log('email', email)
-    const [password, setPassword] = useState("");
-    console.log('password', password)
-
-
-    return (
-        <div className="register">
-            <div className="register-form">
-                <h1 className="form-title display-3">Login</h1>
-                <form id="login">
-                    <div className="mb-3">
-                        <label for="email" className="form-label display-4">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="Enter your email address..." onChange={e => setEmail(e.target.value)}/>
-                    </div>
-                    <div className="mb-3">
-                        <label for="password" className="form-label display-4">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="Enter your password address..." onChange={e => setPassword(e.target.value)}/>
-                    </div>
-                    <input type="button" className="btn btn-primary form-control" value="Login" onClick={null}/>
-                </form>
-                <button className="btn btn-primary form-control" onClick={null} >Continue with Google</button>
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    const user = { email, password };
+    signIn(user.email, user.password);
+    history.push("/");
+  };
+  const handleProviderLogin = () => {
+    signUpProvider();
+    history.push("/");
+  };
+  return (
+    <div className="register">
+      <div className="form-image">
+        <img src={"https://picsum.photos/1200/900"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Login</h1>
+        <form id="login">
+          <div className="mb-3">
+            <label for="email" className="form-label display-4">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email address..."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label for="password" className="form-label display-4">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password..."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div classNameName="link" onClick={() => forgotPassword(email)}>
+              Forgot password?
             </div>
-            <div className="form-image">
-                <img src={"https://picsum.photos/1200/900"} alt="sample" />
-            </div>
-        </div>
-    )
-}
-
-export default Login
+          </div>
+          <input
+            type="button"
+            className="btn btn-primary form-control"
+            value="Login"
+            onClick={handleSubmit}
+          />
+        </form>
+        <button
+          className="btn btn-primary form-control"
+          onClick={handleProviderLogin}
+        >
+          Continue with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Login;
